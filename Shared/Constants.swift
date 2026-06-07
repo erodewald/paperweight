2a17eee46cd1a5aaa787ff310a503aab52b0341d
Eric J. Rodewald <eric.rode@gmail.com>
2026-06-07 17:00:38 -0500

    chore: update bundle IDs and App Group to media.baltar.paperweight

--- Shared/Constants.swift
+++ Shared/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Paperweight {
-    static let appGroupID = "group.com.paperweight.app"
+    static let appGroupID = "group.media.baltar.paperweight"
     static let activityName = "dailySchedule"
     static let storeName = "paperweight"
     static let defaultUnlockDuration: TimeInterval = 15 * 60
